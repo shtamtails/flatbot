dd395fc86a6b13f8ece13817f7936d51740759c6
shtamtails <maximyudin1@gmail.com>
2022-08-16 14:01:23 +0300

    bugfix

--- src/utils/adListener.ts
+++ src/utils/adListener.ts
@@ -1,15 +1,16 @@
 import { Context } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
+import { IListing } from "../models/IListing";
 
 export const setAdListener = async (ctx: Context<Update>, get: Function, send: Function) => {
-  let lastAd = await get();
+  let lastAd: IListing[] = await get();
   await send(ctx, lastAd);
   setInterval(async () => {
-    let newAd = await get();
-    newAd[0].time !== lastAd[0].time && (lastAd = newAd);
+    let newAd: IListing[] = await get();
     if (newAd[0].time !== lastAd[0].time) {
       lastAd = newAd;
       await send(ctx, newAd);
+    } else {
     }
   }, 60000);
 };
